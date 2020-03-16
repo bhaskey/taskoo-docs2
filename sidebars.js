711b7bc33af4c951f46884c8294e9ea065f5f404
anil bhaskar <anilbhaskar.cse@gmail.com>
2020-03-16 21:57:56 +0530

    add task details in sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    API: ["api/post-task", "api/browse-tasks", "api/offer", "api/question"],
+    API: ["api/post-task", "api/browse-tasks", "api/task-details", "api/offer", "api/question"],
     Features: ['mdx'],
   },
 };
